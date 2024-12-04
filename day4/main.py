62a0898209376234f1f95a3f284553d4e58f9579
Timon van der Meulen <timonvandermeulen@MacBook-Pro-van-Timon.local>
2024-12-04 16:08:27 +0100

    Day 4 part 2

--- day4/main.py
+++ day4/main.py
@@ -1,11 +1,7 @@
-class WordSearch():
-
+class CrossWordSearch():
     grid = []
-    searchfor = ''
-    occurences = 0
 
-    def __init__(self, searchfor):
-        self.searchfor = searchfor
+    def __init__(self):
 
         with open('input.txt', encoding="UTF-8", mode="r") as file:
             lines = file.readlines()
@@ -15,26 +11,65 @@ class WordSearch():
                 self.grid.append(list(line.strip()))
 
 
-    def start(self):
+    #Starter for part 1 where we search for a specific string in all directions
+    def part1(self, searchfor):
+        occurences = 0
         for y, row in enumerate(self.grid):
             for x, char in enumerate(row):
                 #Find first char
-                if char == list(self.searchfor)[0]:
+                if char == list(searchfor)[0]:
 
-                    self.occurences += self.find_horizontally(x, y)
-                    self.occurences += self.find_horizontally(x, y, True) #Reverse
-                    self.occurences += self.find_vertically(x, y)
-                    self.occurences += self.find_vertically(x, y, True)
-                    self.occurences += self.find_diagonally(x, y)
-                    self.occurences += self.find_diagonally(x, y, True)
-                    self.occurences += self.find_diagonally_other(x, y)
-                    self.occurences += self.find_diagonally_other(x, y, True)
+                    occurences += self.find_horizontally(searchfor, x, y)
+                    occurences += self.find_horizontally(searchfor, x, y, True) #Reverse
+                    occurences += self.find_vertically(searchfor, x, y)
+                    occurences += self.find_vertically(searchfor, x, y, True)
+                    occurences += self.find_diagonally(searchfor, x, y)
+                    occurences += self.find_diagonally(searchfor, x, y, True)
+                    occurences += self.find_diagonally_other(searchfor, x, y)
+                    occurences += self.find_diagonally_other(searchfor, x, y, True)
 
-        print(f"The amount of {self.searchfor} in the wordsearch is {self.occurences}")
+        print(f"The amount of {searchfor} in the wordsearch is {occurences}")
+
+    #Starter for part 2 where we search for a crosses MAS (X-MAS)
+    def part2(self):
+        occurences = 0
+        for y, row in enumerate(self.grid):
+            for x, char in enumerate(row):
+
+                #Start with the A
+                if char == "A":
+                    #Get the characters around the A in a cross
+                    cross = self.get_cross(x,y)
+                    #Check output if it matches. Chars go left top, right top, right bottom,
+                    # left bottom
+                    if cross in ['MMSS', 'SMMS', 'SSMM', 'MSSM']:
+                        #print(f"Found X-MAS cross on {x},{y} with cross {cross}")
+                        occurences += 1
+
+        print(f"Found a whopping total of X-MAS crosses: {occurences}")
+
+
+    #Get cross letters from starting point
+    def get_cross(self, x, y):
+        try:
+            #Cannot find cross along left border, others are handled by out of bounds issues
+            if y == 0:
+                raise IndexError()
+
+            result = [
+                self.grid[y-1][x-1],
+                self.grid[y-1][x+1],
+                self.grid[y+1][x+1],
+                self.grid[y+1][x-1]
+            ]
+        except IndexError:
+            return ""
+
+        return "".join(result)
 
     #Find searchfor horizontally from starting position
-    def find_horizontally(self, x, y, rev = False):
-        for index, char in enumerate(list(self.searchfor)):
+    def find_horizontally(self, searchfor, x, y, rev = False):
+        for index, char in enumerate(list(searchfor)):
             try:
                 newx = x+(index if not rev else index * -1)
                 if newx < 0:
@@ -45,12 +80,12 @@ class WordSearch():
             except IndexError:
                 return False
 
-        print(f"Found {self.searchfor} horizontally{' (reverse)' if rev else ''} on {x},{y}")
+        #print(f"Found {searchfor} horizontally{' (reverse)' if rev else ''} on {x},{y}")
         return True
 
     #Find searchfor vertically from starting position
-    def find_vertically(self, x, y, rev = False):
-        for index, char in enumerate(list(self.searchfor)):
+    def find_vertically(self, searchfor, x, y, rev = False):
+        for index, char in enumerate(list(searchfor)):
             try:
                 newy = y+(index if not rev else index * -1)
                 if newy < 0:
@@ -61,12 +96,12 @@ class WordSearch():
             except IndexError:
                 return False
 
-        print(f"Found {self.searchfor} vertically{' (reverse)' if rev else ''} on {x},{y}")
+        #print(f"Found {searchfor} vertically{' (reverse)' if rev else ''} on {x},{y}")
         return True
 
     #Find searchfor diagonally (top-left to bottom-right) from starting position
-    def find_diagonally(self, x, y, rev = False):
-        for index, char in enumerate(list(self.searchfor)):
+    def find_diagonally(self, searchfor, x, y, rev = False):
+        for index, char in enumerate(list(searchfor)):
             try:
                 newx = x+(index if not rev else index * -1)
                 newy = y+(index if not rev else index * -1)
@@ -78,13 +113,13 @@ class WordSearch():
             except IndexError:
                 return False
 
-        print(f"Found {self.searchfor} diagonally{' (reverse)' if rev else ''} on {x},{y}")
+        #print(f"Found {searchfor} diagonally{' (reverse)' if rev else ''} on {x},{y}")
         return True
 
     #TODO: Better name for this one?
     #Find searchfor diagonally (bottom-left to top-right) from starting position
-    def find_diagonally_other(self, x, y, rev = False):
-        for index, char in enumerate(list(self.searchfor)):
+    def find_diagonally_other(self, searchfor, x, y, rev = False):
+        for index, char in enumerate(list(searchfor)):
             try:
                 newx = x-(index if not rev else index * -1)
                 newy = y+(index if not rev else index * -1)
@@ -96,10 +131,11 @@ class WordSearch():
             except IndexError:
                 return False
 
-        print(f"Found {self.searchfor} diagonally other{' (reverse)' if rev else ''} on {x},{y}")
+        #print(f"Found {searchfor} diagonally other{' (reverse)' if rev else ''} on {x},{y}")
         return True
 
 
 if __name__=="__main__":
-    finder = WordSearch('XMAS')
-    finder.start()
+    cws = CrossWordSearch()
+    cws.part1('XMAS')
+    cws.part2()
